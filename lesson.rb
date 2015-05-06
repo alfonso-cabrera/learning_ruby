57e687773335ad480d495efb91790966abcd9f54
alfonso-cabrera <a.l.cabrera11@gmail.com>
2015-05-06 23:08:15 +0000

    new stuff

---
+++ lesson.rb
@@ -0,0 +1,17 @@
+array1 = [1,2,3]
+hash1 = {"test" => "pass"}
+
+hash["y"]
+
+array[0]
+
+array.push("strawberry")
+
+atlanta_teams[:basketball] = "hawks"
+
+num_array[2][1]
+
+atlanta_teams["players"]["pf"]
+
+
+
